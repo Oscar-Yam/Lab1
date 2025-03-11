54a46d548ddb1756e948d2f18938ace6cdc16c18
Oscar <oscar0222dm@gmail.com>
2025-03-11 20:57:09 +0800

    commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+const App = () => {
+  const navigation = useNavigation();
+  const handleLogin = () => {
+    // Navigate to Home screen
+    //console.log("logged in");
+    navigation.navigate('Home');
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login From</Text>
+      <TextInput style={styles.input}
+        placeholder='Email'/>
+      <TextInput style={styles.input}
+        placeholder='Password'
+        secureTextEntry/>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#533551',
   },
+  title:{
+    fontSize:32,
+    fontWeight:'bold',
+    color:'#f5f55f',
+    marginBottom:16,
+  },
+  input:{
+    width:"80%",
+    height:50,
+    borderColor:'#f5f55f',
+    borderWidth:2,
+    borderRadius:10,
+    paddingHorizontal:10,
+    backgroundColor:'#fff'
+  },
+  button:{
+    width:"80%",
+    height:50,
+    backgroundColor:'#007bff',
+    justifyContent:"center",
+    alignItems:'center',
+    borderRadius:20,
+  },
+  buttonText:{
+    color:'#fff',
+    fontSize:18,
+    fontWeight:"bold",
+  }
 });
+
+export default App;
